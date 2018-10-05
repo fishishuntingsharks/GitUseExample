dfb8d68e2937fc7760d343ca7c4ff65cea589c91
Daniel <dani.kiesel@gmail.com>
2018-10-05 11:31:12 +0200

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Kiesel Daniel on 05.10.18.
+//  Copyright © 2018 Kiesel Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
