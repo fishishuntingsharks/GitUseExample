9fbfa5e3229ba47847972847a0a6c3b515aaabca
Daniel <dani.kiesel@gmail.com>
2018-10-05 12:07:45 +0200

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Kiesel Daniel on 05.10.18.
+//  Copyright © 2018 Kiesel Daniel. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary : NSObject {
+    
+}
